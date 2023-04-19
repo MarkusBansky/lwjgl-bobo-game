29f07fcbe7d751154ede79eb878be999d6c1aeef
Markiian Benovskyi <MarkusBansky@users.noreply.github.com>
2023-04-19 23:30:29 +0100

    Initial commit

---
+++ src/main/java/com/markiian/benovskyi/bobo/properties/WindowProperties.java
@@ -0,0 +1,7 @@
+package com.markiian.benovskyi.bobo.properties;
+
+public final class WindowProperties {
+    public static final String WINDOW_TITLE = "Bobo";
+    public static final int WINDOW_WIDTH = 800;
+    public static final int WINDOW_HEIGHT = 600;
+}
